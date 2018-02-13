e988a0ba3454fb23f710be2908ad420172d9d012
Ettiene Mare <ettienemare@gmail.com>
2018-02-13 22:35:21 +0200

    Add nova core and analytics

--- src/app/modules/analytics/analytics.module.ts
+++ src/app/modules/analytics/analytics.module.ts
@@ -1,23 +1,33 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import {ModuleWithProviders, NgModule, Provider} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AnalyticsService} from "./services/analytics.service";
 import {LocalizationService} from "./services/localization.service";
 import {AnalyticsClient} from "./services/analytics.client.service";
+import {NovaCoreLibModule, NovaHttpClient} from "nova-core-lib";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NovaCoreLibModule.forRoot()
   ],
   declarations: []
 })
 export class AnalyticsModule {
-  public static forRoot(...configs: Array<any>): ModuleWithProviders {
+  public static forRoot(
+    providedNovaHttpClient: Provider = {
+      provide: NovaHttpClient,
+      useClass: NovaHttpClient
+    },
+    ...configs: Array<any>
+  ): ModuleWithProviders {
+
     for(let config of configs)
       AnalyticsService.addEvents(config.getEvents());
 
     return {
       ngModule: AnalyticsModule,
       providers: [
+      //  providedNovaHttpClient,
         AnalyticsService,
         AnalyticsClient,
         LocalizationService
